c185e403cc724925dfe608901c823a3964215883
Luis david <david12.29@hotmail.com>
2020-06-15 10:34:42 -0500

    Estoy arreglando las rutas

--- front-end/js/veterinarias.js
+++ front-end/js/veterinarias.js
@@ -6,40 +6,45 @@ const indice = document.getElementById('indice');
 const form = document.getElementById('form');
 const btnGuardar = document.getElementById('btn-guardar');
 const listaVeterinarias = document.getElementById('lista-veterinarias');
+const url="http://localhost:5000/veterinarias"
+let veterinarias = [];
 
-let veterinarias = [
-  {
-    nombre: "Naryie",
-    apellido: "Vasquez",
-    pais: "Colombia",
-    identificacion: "1234567890"
-  },
-  {
-    nombre: "Juan David",
-    apellido: "Marín",
-    pais: "Colombia",
-    identificacion: "1234567899"
+
+async function listarVeterinarias() {
+  try {
+    const respuesta = await fetch(url);
+    const veterinariasDelServer = await respuesta.json();
+    if (Array.isArray(veterinariasDelServer)) {
+      veterinarias = veterinariasDelServer;
+    }
+    if(veterinarias.length>0){
+      const htmlVeterinarias = veterinarias.map((veterinaria, index)=>`<tr>
+          <th scope="row">${index}</th>
+          <td>${veterinaria.identificacion}</td>
+          <td>${veterinaria.pais}</td>
+          <td>${veterinaria.nombre}</td>
+          <td>${veterinaria.apellido}</td>
+          <td>
+              <div class="btn-group" role="group" aria-label="Basic example">
+                  <button type="button" class="btn btn-info editar"><i class="fas fa-edit"></i></button>
+                  <button type="button" class="btn btn-danger eliminar"><i class="far fa-trash-alt"></i></button>
+              </div>
+          </td>
+        </tr>`).join("");
+        listaVeterinarias.innerHTML = htmlVeterinarias;
+        Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index)=>botonEditar.onclick = editar(index));
+        Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar, index)=>botonEliminar.onclick = eliminar(index)
+        );
+        return;
+    }
+    listaVeterinarias.innerHTML = `<tr>
+        <td colspan="5" class="lista-vacia">No hay veterinarias</td>
+      </tr>`;  
+  } catch (error) {
+    console.log({error});
+    $("alert").show();
   }
-];
-
-
-function listarVeterinarias() {
-  const htmlVeterinarias = veterinarias.map((veterinaria, index)=>`<tr>
-      <th scope="row">${index}</th>
-      <td>${veterinaria.identificacion}</td>
-      <td>${veterinaria.pais}</td>
-      <td>${veterinaria.nombre}</td>
-      <td>${veterinaria.apellido}</td>
-      <td>
-          <div class="btn-group" role="group" aria-label="Basic example">
-              <button type="button" class="btn btn-info editar"><i class="fas fa-edit"></i></button>
-              <button type="button" class="btn btn-danger eliminar"><i class="far fa-trash-alt"></i></button>
-          </div>
-      </td>
-    </tr>`).join("");
-    listaVeterinarias.innerHTML = htmlVeterinarias;
-    Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index)=>botonEditar.onclick = editar(index));
-    Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar, index)=>botonEliminar.onclick = eliminar(index));
+  
 }
 
 function enviarDatos(evento) {
